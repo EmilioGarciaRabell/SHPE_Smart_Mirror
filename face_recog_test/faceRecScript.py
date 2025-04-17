356ebce86663023a3be64046044951f6fb23dda6
Antonios Stavrou <antoniosstavroulopez@gmail.com>
2025-04-17 00:08:07 -0400

    made some more progress with facial recognition script

--- face_recog_test/faceRecScript.py
+++ face_recog_test/faceRecScript.py
@@ -23,6 +23,44 @@ class faceRec:
             self.knownFaceNames.append(image)
         print(self.knownFaceNames)
 
+    def runFR(self):
+        webcamVideo = cv2.VideoCapture(0)
+        if not webcamVideo.isOpened():
+            sys.exit('Webcam not found!')
+        while True:
+            ret, frame = webcamVideo.read()
+            if self.procActualFace:
+                smallFrame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+                rgbSmallFrame = smallFrame[:, :, ::-1]
+                self.faceLocations = face_recognition.face_locations(rgbSmallFrame)
+                self.faceEncodings = face_recognition.face_encodings(rgbSmallFrame, self.faceLocations)
+                self.faceNames = []
+                for faceEncoding in self.faceEncodings:
+                    matches =  face_recognition.compare_faces(self.knownFaceEncodings, faceEncoding)
+                    name = "Unknown"
+                    confidence = "Unknown"
+                    faceDistances = face_recognition.face_distance(self.knownFaceEncodings, faceEncoding)
+                    bestMatchIndex = np.argmin(faceDistances)
+                    if matches[bestMatchIndex]:
+                        name = self.knownFaceNames[bestMatchIndex]
+                        confidence = faceMatchPercentage(faceDistances[bestMatchIndex])
+                    self.faceNames.append(f'{name} ({confidence})')
+                self.procActualFace = not self.procActualFace
+                for(top, right, bottom, left), name in zip(self.faceLocations, self.faceNames):
+                    top *= 4
+                    right *= 4
+                    bottom *= 4
+                    left *= 4
+
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255, 2))
+                    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), -1)
+                    cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+                cv2.imshow('Face Recognition', frame)
+                if cv2.waitKey(1) == ord('q'):
+                    break
+            webcamVideo.release()
+            cv2.destroyAllWindows()
+
 
 def faceMatchPercentage(faceDistance, faceTH=0.60):
     range = 1.0 - faceTH
@@ -36,7 +74,8 @@ def faceMatchPercentage(faceDistance, faceTH=0.60):
 
 if __name__ == '__main__':
     facialRecognition = faceRec()
-    
+    facialRecognition.runFR()
+
     
 
 
