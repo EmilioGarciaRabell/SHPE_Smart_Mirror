71bf16f84a9f01cfa9de5d4423ca04d3ddd86431
Antonios Stavrou <antoniosstavroulopez@gmail.com>
2025-04-19 14:48:15 -0400

    idk

--- front/client/vite.config.js
+++ front/client/vite.config.js
@@ -2,18 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-//export default defineConfig({
-  //plugins: [react()],
-//})
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    // add your Pi’s DNS name here (or use 'all' to disable host checks)
-    allowedHosts: ['rpi4bw.student.rit.edu']
-    // allowedHosts: 'all'
-  }
 })
