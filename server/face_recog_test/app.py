3ca5fab08fb0b998400181b9ad04a91ad153376b
Antonios Stavrou <antoniosstavroulopez@gmail.com>
2025-04-22 14:15:47 -0400

    fixed countdown bug

--- server/face_recog_test/app.py
+++ server/face_recog_test/app.py
@@ -33,8 +33,9 @@ def registerUser():
             return jsonify({"error": "Webcam not found"}), 500
         t.sleep(2)
         for _ in range(5):
-            cam.release()
+            cam.read()
         ret, frame = cam.read()
+        cam.release()
         if not ret:
             return jsonify({"error": "Failed to capture image"}), 500
         cv2.imwrite(picturePath, frame)
