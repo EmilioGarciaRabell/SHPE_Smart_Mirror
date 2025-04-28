9cd559b009ae6864594987c57f7ec173a08559d4
Antonios Stavrou <antoniosstavroulopez@gmail.com>
2025-04-28 19:44:03 -0400

    added some files and made a bash script to start front and back.

---
+++ startFront_Back.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Go to the backend directory
+cd ~/SHPE_Smart_Mirror/server || exit 1
+
+# Activate Python virtual environment
+#source py3.9env/bin/activate
+
+# Start the backend in the background
+echo "Starting Flask backend..."
+python app.py &
+BACKEND_PID=$!
+
+# Go to the frontend directory
+cd ../frontend || exit 1
+
+# Start the frontend
+echo "Starting Vite frontend..."
+npm run dev
+
+# If frontend stops, kill backend
+echo "Stopping backend..."
+kill $BACKEND_PID
