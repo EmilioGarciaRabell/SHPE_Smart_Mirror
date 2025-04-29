431876b55eabccce93da1cf9afc03b221480d388
Antonios Stavrou <antoniosstavroulopez@gmail.com>
2025-04-28 21:15:08 -0400

    bash script edits

--- start_mirror.sh
+++ start_mirror.sh
@@ -4,6 +4,8 @@
 echo "Killing any process using port 5000..."
 fuser -k 5000/tcp
 
+export VITE_API_URL="http://localhost:5000"
+
 # Start the backend (Flask app) in the background
 echo "Starting backend..."
 cd ~/SHPE_Smart_Mirror/server || exit 1
@@ -14,11 +16,12 @@ cd ..
 # Start the frontend (Vite) in the background
 echo "Starting frontend..."
 cd ~/SHPE_Smart_Mirror/front/client || exit 1
-npm run dev &
+npm install
+npm run build &
 FRONTEND_PID=$!
 
 # Give the frontend a few seconds to fully start
-sleep 5
+sleep 8
 
 # Open Chromium in fullscreen to localhost:5173
 echo "Opening Chromium in fullscreen mode..."
